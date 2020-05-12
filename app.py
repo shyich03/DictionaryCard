9d6a162421eba97cf43635f0ac3bc8b394ecd762
Tom Shan <shyich03@hotmail.com>
2020-05-12 10:31:08 -0400

    height center

--- app.py
+++ app.py
@@ -27,18 +27,18 @@ def GetTextDimensions(text, points, font):
     return size
 
 
-def TransformText(text, point, font):
+def TransformText(text, point, font, width=0):
     # find line width
     print(text)
     print("total width", GetTextDimensions(text, point, font)[0] )
-    width = GetTextDimensions(text, point, font)[0] < 1200 and 460 or 600
+    width = width == 0 and(GetTextDimensions(text, point, font)[0] < 1500 and 460 or 600) or width
     res = []
     cur = ""
     for word in text.split(" "):
         temp = cur + word + " "
         if GetTextDimensions(temp, point, font)[0]>width:
             res += [cur]
-            cur = word
+            cur = word+" "
         else:
             cur = temp
     if cur!="":
@@ -56,46 +56,64 @@ def GetFont(text, initPoint, maxLength, font):
     return (point, lineLength)
 if __name__ == "__main__":
 
+    titleSize = 100
+    maxTitleWidth = 630
+    descSize = 32
+    linebreak = 7
     step = 0
+    count = 1
     with open('text.txt') as file:
         for line in file:
             if step == 0:
                 #image size/color
-                img = Image.new('RGB', (680, 680), color = (233, 235, 232))
+                # img = Image.new('RGB', (680, 680), color = (233, 235, 232))
+                img = Image.open("background.jpg")
+                img = img.resize((680,680))
                 d = ImageDraw.Draw(img)
-                fontSize, lineLength = GetFont(line, 60, 650, "Dubai-Medium")
-                posX = (690-lineLength)/2
-                posY = 20
-                font = ImageFont.truetype('fonts/Dubai-Medium.ttf', fontSize)
-                pos = (posX,posY)
-                title = line[len(line)-2]=="." and line.rsplit(' ', 1)[0] or line[0:len(line)-1]
-                d.text(pos, line, font=font, fill=(0, 0, 1))
+                fontSize, lineLength = GetFont(line, titleSize, maxTitleWidth, "Dubai-Medium")
+                titleFont = ImageFont.truetype('fonts/Dubai-Medium.ttf', fontSize)
+                # title = line[len(line)-2]=="." and line.rsplit(' ', 1)[0] or line[0:len(line)-1]
+                # d.text(pos, line, font=font, fill=(0, 0, 1))
+                title = line
+                titleX = (720-lineLength)/2
+                titleY = 0
             elif step == 1:
-                posY = 120
-                width,texts = TransformText(line, 24, 'Dubai-Regular')
-                font = ImageFont.truetype('fonts/Dubai-Regular.ttf', 24)
-                print(width, texts)
-                posX = (690-width)/2
-                for text in texts:
+                posY = 165
+                width,descText = TransformText(line, descSize, 'Dubai-Regular')
+                descFont = ImageFont.truetype('fonts/Dubai-Regular.ttf', descSize)
+                descX = (690-width)/2
+                descY = []
+                for text in descText:
                     if text!="\n ":
-                        pos = (posX,posY)
-                        d.text(pos, text, font=font, fill=(0, 0, 1))
-                        posY+=27
+                        # d.text(pos, text, font=font, fill=(0, 0, 1))
+                        descY += [posY]
+                        posY+=descSize+linebreak
             elif step == 2:
-                posY += 30
-                texts = TransformText(line, 24, 'Dubai-Light')[1]
-                posX = (690-width)/2
-                font = ImageFont.truetype('fonts/Dubai-Light.ttf', 24)
-                print(width, texts)
-                for text in texts:
+                posY += descSize+linebreak
+                exampleText = TransformText(line, descSize, 'Dubai-Light', width)[1]
+                exampleX = (690-width)/2
+                exampleY = []
+                exampleFont = ImageFont.truetype('fonts/Dubai-Light.ttf', descSize)
+                for text in exampleText:
                     if text!="\n ":
-                        pos = (posX,posY)
-                        d.text(pos, text, font=font, fill=(0, 0, 1))
-                        posY+=27
+                        # pos = (posX,posY)
+                        # d.text(pos, text, font=font, fill=(0, 0, 1))
+                        exampleY += [posY]
+                        posY+=descSize+linebreak
             else:
                 if line == "\n":
-                    mewImg = img.crop((0, 0,680, posY+55))
-                    mewImg.save('img/'+slugify(title)+'.png')
+                    baseHeight = (620-posY)/2
+                    d.text((titleX,titleY+baseHeight), title, font=titleFont, fill=(0, 0, 1))
+                    for i in range(len(descText)):
+                        text = descText[i]
+                        if text!="\n ":
+                            d.text((descX, descY[i]+baseHeight), text, font=descFont, fill=(0, 0, 1))
+                    for i in range(len(exampleText)):
+                        text = exampleText[i]
+                        if text!="\n ":
+                            d.text((exampleX, exampleY[i]+baseHeight), text, font=exampleFont, fill=(0, 0, 1))
+                    img.save('img/'+str(count)+'.png')
+                    count+=1
                 else:
                     print(line)
                     print ("Error with text file while create png for ",title)
